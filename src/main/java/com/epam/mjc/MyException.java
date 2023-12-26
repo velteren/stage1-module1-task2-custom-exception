3c657960b29cfab298fe891c9e0ad79a468539f3
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-26 13:21:28 +0300

    feat: add solution

--- src/main/java/com/epam/mjc/MyException.java
+++ src/main/java/com/epam/mjc/MyException.java
@@ -1,11 +1,7 @@
 package com.epam.mjc;
 
-public class MyException extends IllegalArgumentException {
-    private int detail;
-    MyException(int detail) {
-        this.detail = detail;
-    }
-    public String toString() {
-        return "Could not find student with ID " + detail;
+public class MyException extends IllegalArgumentException{
+    public MyException(String message) {
+        super(message);
     }
 }
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,23 +5,22 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) throws MyException {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws MyException{
+    for (long id : IDs) {
+      if (id==studentID)
+        return Student.getValueOf(studentID);
+    }
+    throw new MyException("Could not find student with ID " + studentID);
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      try {
-        Student student = manager.find(IDs[i]);
-        System.out.println("Student name " + student.getName());
-      } catch (MyException e) {
-        System.out.println(e.toString());
-      } catch (NullPointerException e) {
-        System.out.println("Could not find student with ID Null");
-      }
+      Student student = manager.find(IDs[i]);
+      System.out.println("Student name " + student.getName());
     }
 
   }
+
 }
